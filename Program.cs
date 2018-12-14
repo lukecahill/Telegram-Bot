88ea835f2798ebc79e4af4e8bd3ac0d1a08dea9f
Luke Cahill <lmc@outlook.com>
2018-12-14 14:48:02 +0000

    Add inital on message recieve

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace TelegramBot {
+    class Program {
+        static ITelegramBotClient botClient;
+        static string token = Authentication.token;
+
+        static void Main() {
+        botClient = new TelegramBotClient(token);
+
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+            Thread.Sleep(int.MaxValue);
+        }
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e) {
+            if (e.Message.Text != null) {
+                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+                await botClient.SendTextMessageAsync(
+                    chatId: e.Message.Chat,
+                    text:   "You said:\n" + e.Message.Text
+                );
+            }
+        }
+    }
+}
