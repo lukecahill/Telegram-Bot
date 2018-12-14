eb96dab685226348b000864035db96b70d72f6d7
Luke Cahill <lmc@outlook.com>
2018-12-14 18:54:31 +0000

    Begin adding functionality

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using Microsoft.EntityFrameworkCore;
 
 namespace TelegramBot {
     class Program {
         static ITelegramBotClient botClient;
         static string token = Authentication.token;
+        static List<Items> itemList = new List<Items>();
 
         static void Main() {
         botClient = new TelegramBotClient(token);
@@ -23,11 +27,64 @@ namespace TelegramBot {
             if (e.Message.Text != null) {
                 Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
 
+                string message = e.Message.Text.Trim();
+                string reply = "";
+
+                if(message.StartsWith("/help")) {
+                    reply = sendHelp();
+                } else if(message.StartsWith("/remove")) {
+                    reply = removeItem(message);
+                } else if(message.StartsWith("/add")) {
+                    reply = addItem(message);
+                } else if(message.StartsWith("/get")) {
+                    reply = getItems();
+                } else if(message.StartsWith("/clear")) {
+                    reply = getItems();
+                } else {
+                    reply = "Unknown option.";
+                }
+
                 await botClient.SendTextMessageAsync(
                     chatId: e.Message.Chat,
-                    text:   "You said:\n" + e.Message.Text
+                    text: reply
                 );
             }
+        }
+
+        static string removeItem(string message) {
+            var messageItem = message.Substring(7).TrimStart();
+            var itemToRemove = itemList.Find(x => x.name == messageItem);
+            itemList.Remove(itemToRemove);
+
+            return $"Removing {itemToRemove.name}";
+        }
+
+        static string addItem(string message) {
+            var messageItem = message.Substring(4).TrimStart();
+            var item = new Items {
+                name = messageItem,
+                itemId = 0
+            };
+
+            itemList.Add(item);
+            return $"Adding {item.name}";
+        }
+
+        static string sendHelp() {
+            return "Good luck";
+        }
+
+        static string clearItems() {
+            itemList.Clear();
+            return "All items have been removed from the list";
+        }
+
+        static string getItems() {
+            var toReturn = "These are the items on your list:\n\n";
+            for(var i = 0; i < itemList.Count; i++) {
+                toReturn += "*>*" + itemList.ElementAt(i).name + "\n";
+            }
+            return toReturn;
         }
     }
 }
