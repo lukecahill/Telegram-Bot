a94393cbbd065d06e8c330be7fffcc49749de2fe
Luke Cahill <lmc@outlook.com>
2018-12-26 16:17:56 +0000

    Add annotations to item model and create initial migration

---
+++ Migrations/20181226161059_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace telegram_bot.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    itemId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.itemId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
--- models/Items.cs
+++ models/Items.cs
@@ -1,4 +1,9 @@
-class Items {
-    public int itemId { get; set; }
-    public string name { get; set; }
+using System.ComponentModel.DataAnnotations;
+
+namespace TelegramBot {
+    class Items {
+        [Key]
+        public int itemId { get; set; }
+        public string name { get; set; }
+    }
 }
